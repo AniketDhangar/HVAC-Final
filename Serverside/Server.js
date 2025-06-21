b55584b1ed0e1bc156594e311f7277a8e74ed2e0
AniketDhangar <21aniketdhangar13@gmail.com>
2025-06-21 19:28:23 +0530

    Fix: Removed trailing slash in CORS origin

--- Serverside/Server.js
+++ Serverside/Server.js
@@ -24,7 +24,7 @@ app.use(helmet())
 
 // Middleware
 app.use(cors({
-  origin: 'https://hvac-technical.netlify.app/',
+  origin: 'https://hvac-technical.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
